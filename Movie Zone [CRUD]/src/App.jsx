35b54e1c9a1e13f783e5297bbf163760e85429b9
SinerSins <sinerssins@gmail.com>
2025-01-28 18:18:29 +0530

    update

--- Movie Zone [CRUD]/src/App.jsx
+++ Movie Zone [CRUD]/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { AddMovie } from './component/addMovie'
+import { AddMovie } from './component/AddMovie'
 import './App.css'
 import { Route,Routes } from 'react-router-dom'
 import { MovieList } from './component/MovieList'
